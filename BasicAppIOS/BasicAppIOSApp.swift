71a6b719bf5f2062eb8cec0ac421c4d8ca7b5ce4
Emeric Espiritu <emericespiritusantiago@gmail.com>
2024-10-05 11:09:24 -0500

    Initial Commit

---
+++ BasicAppIOS/BasicAppIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicAppIOSApp.swift
+//  BasicAppIOS
+//
+//  Created by Emeric on 5/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BasicAppIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
